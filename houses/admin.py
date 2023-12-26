2dbc684458a4f5cb17f8041b3263a027f63f7df4
zsert1 <zsert1@naver.com>
2023-12-26 10:30:02 -0500

    admin 패널

--- houses/admin.py
+++ houses/admin.py
@@ -4,4 +4,13 @@ from .models import House
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+   list_display=[
+      "name",
+      "price_per_night",
+      "address",
+      "pets_allow"
+   ]
+   list_filter=["price_per_night",'pets_allow']
+   search_fields=["address"]
+   list_display_links=("name","address")
+   list_editable=("pets_allow",)
--- houses/models.py
+++ houses/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class House(models.Model):
     """ house Model """
     name=models.CharField(max_length=140)
-    price_per_night=models.PositiveIntegerField()
+    price_per_night=models.PositiveIntegerField(verbose_name="Price",help_text="Positive Numbers Only")
     descriotion=models.TextField()
     address=models.CharField(max_length=140)
-    pets_allow=models.BooleanField(default=True)
+    pets_allow=models.BooleanField(verbose_name="Pets Allowed", default=True,help_text="Does this house allow pets?")
+
+    def __str__(self):
+        return self.name
