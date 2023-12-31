286e564499ed2b7612b8df92fa529bf6be080d0e
zsert1 <zsert1@naver.com>
2023-12-31 09:41:34 -0500

    ForgineKey

--- houses/models.py
+++ houses/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+
 class House(models.Model):
-    """ house Model """
-    name=models.CharField(max_length=140)
-    price_per_night=models.PositiveIntegerField(verbose_name="Price",help_text="Positive Numbers Only")
-    descriotion=models.TextField()
-    address=models.CharField(max_length=140)
-    pets_allow=models.BooleanField(verbose_name="Pets Allowed", default=True,help_text="Does this house allow pets?")
+    """house Model"""
+
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Numbers Only"
+    )
+    descriotion = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allow = models.BooleanField(
+        verbose_name="Pets Allowed",
+        default=True,
+        help_text="Does this house allow pets?",
+    )
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
