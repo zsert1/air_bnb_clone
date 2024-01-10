89299591c3e32b410d1abaddac7882c44ba9595e
zsert1 <zsert1@naver.com>
2024-01-10 23:57:48 +0900

    urls and view start

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rooms import views as room_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("rooms", room_views.say_hello),
 ]
