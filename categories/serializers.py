f3ea754b9a964fd46f43973a2167374486629691
zsert1 <zsert1@naver.com>
2024-01-17 23:27:46 +0900

    is_vaild

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 
 class CategorySerializer(serializers.Serializer):
     # 어떤 필드에 대해서 노출할지 작성
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created = serializers.DateTimeField()
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    created = serializers.DateTimeField(read_only=True)
