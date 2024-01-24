1e79ec5c70c2ff0cfdb223464ce3a9767c01dcf2
zsert1 <zsert1@naver.com>
2024-01-24 22:14:53 +0900

    APIVIEW

--- categories/views.py
+++ categories/views.py
@@ -4,19 +4,17 @@ from rest_framework.response import Response
 from .serializers import CategorySerializer
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_202_ACCEPTED
+from rest_framework.views import APIView
+
 
 # Create your views here.
-
-
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
+class Categories(APIView):
+    def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
-        return Response(
-            serializer.data,
-        )
-    elif request.method == "POST":
+        return Response(serializer.data)
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             new_category = serializer.save()
@@ -27,18 +25,20 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, pk):
         serializer = CategorySerializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             partial=True,
         )
@@ -47,6 +47,7 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_202_ACCEPTED)
