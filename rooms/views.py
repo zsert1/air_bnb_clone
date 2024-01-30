41099025d02483ae74f0467d6e3fd84140930de6
zsert1 <zsert1@naver.com>
2024-01-30 21:49:10 +0900

    api start

--- rooms/views.py
+++ rooms/views.py
@@ -1,32 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework.views import APIView
+from .models import Amenity
+from .serializers import AmenitySerializer
+from rest_framework.response import Response
 
 
-# Create your views here.
-def see_all_rooms(request):
-    rooms = Room.objects.all()
-    return render(
-        request,
-        "all_rooms.html",
-        {
-            "rooms": rooms,
-            "title": "Hello! this title comes from django!",
-        },
-    )
+class Amenities(APIView):
 
-
-def see_one_room(request, room_pk):
-    try:
-        room = Room.objects.get(pk=room_pk)
-        return render(
-            request,
-            "room_detail.html",
-            {"room": room},
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(
+            all_amenities,
+            many=True,
         )
-    except Room.DoesNotExist:
-        return render(
-            request,
-            "room_detail.html",
-            {"not_found": True},
-        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(
+                AmenitySerializer(amenity).data,
+            )
+
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
