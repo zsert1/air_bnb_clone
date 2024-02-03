0f02d4bb417fb8bf6d089bb58c9c6b8b8b8c0697
zsert1 <zsert1@naver.com>
2024-02-03 21:34:56 +0900

    api detail

--- rooms/views.py
+++ rooms/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from .models import Amenity
 from .serializers import AmenitySerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 
 class Amenities(APIView):
@@ -27,11 +29,33 @@ class Amenities(APIView):
 
 
 class AmenityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
     def get(self, request, pk):
-        pass
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity)
+        return Response(serializer.data)
 
     def put(self, request, pk):
-        pass
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(
+            amenity,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            update_amenity = serializer.save()
+            return Response(
+                AmenitySerializer(update_amenity).data,
+            )
+        else:
+            return Response(serializer.errors)
 
     def delete(self, request, pk):
-        pass
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
