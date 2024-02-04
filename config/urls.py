1b9ce2ab8f158afb7d48aef044ea734e29936c59
zsert1 <zsert1@naver.com>
2024-02-04 19:29:54 +0900

    perk api

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/rooms/", include("rooms.urls")),
     path("api/v1/categories/", include("categories.urls")),
+    path("api/v1/experiences/", include("experiences.urls")),
 ]
--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import PerkSerializer
+from .models import Perk
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
 
-# Create your views here.
+
+class Perks(APIView):
+
+    def get(self, request):
+        all_perks = Perk.objects.all()
+        serializer = PerkSerializer(
+            all_perks,
+            many=True,
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PerkSerializer(data=request.data)
+        if serializer.is_valid():
+            perk = serializer.save()
+            return Response(
+                PerkSerializer(perk).data,
+            )
+        else:
+            return Response(PerkSerializer.errors)
+
+
+class PerkDetail(APIView):
+    def get_perk(self, pk):
+        try:
+            return Perk.objects.get(pk=pk)
+        except Perk.DoesNotExist:
+            return NotFound
+
+    def get(self, request, pk):
+        perk = self.get_perk(pk)
+        serializer = PerkSerializer(perk)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        perk = self.get_perk(pk)
+        serializer = PerkSerializer(
+            perk,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            update_perk = serializer.save()
+            return Response(
+                PerkSerializer(update_perk).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        perk = self.get_perk(pk)
+        perk.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
