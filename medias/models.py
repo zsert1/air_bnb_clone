559d14b854fb023528da9180cd12cf4dc461148c
zsert1 <zsert1@naver.com>
2024-03-05 22:34:09 +0900

    usluploads

--- medias/models.py
+++ medias/models.py
@@ -3,7 +3,7 @@ from common.models import CommonModel
 
 
 class Photo(CommonModel):
-    file = models.ImageField()
+    file = models.URLField()
     description = models.CharField(max_length=140)
     room = models.ForeignKey(
         "rooms.Room",
@@ -26,7 +26,7 @@ class Photo(CommonModel):
 
 
 class Video(CommonModel):
-    file = models.FileField()
+    file = models.URLField()
     experience = models.OneToOneField(
         "experiences.Experiences",
         on_delete=models.CASCADE,
--- rooms/views.py
+++ rooms/views.py
@@ -13,6 +13,7 @@ from categories.models import Category
 from django.db import transaction
 from reviews.seriailzers import ReviewSerialzers
 from django.conf import settings
+from medias.serializers import PhotoSerializers
 
 
 class Amenities(APIView):
@@ -185,6 +186,23 @@ class RoomReviews(APIView):
 
 
 class RoomPhotos(APIView):
+    def getObject(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
 
     def post(self, request, pk):
-        pass
+        room = self.getObject(pk)
+
+        if not request.user.is_authenticated:
+            raise NotAuthenticated
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializers(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializers(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
